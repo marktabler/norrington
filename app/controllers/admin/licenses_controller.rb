3cc99ceea85344e02fae3c14af80ffbe067dd9fa
Mark Tabler <mark.tabler@fallingmanstudios.net>
2013-06-09 22:45:10 -0700

    Add admin controls for licenses

--- app/controllers/admin/licenses_controller.rb
+++ app/controllers/admin/licenses_controller.rb
@@ -1,5 +1,20 @@
 class Admin::LicensesController < ApplicationController
 
   before_filter :admin_required
-  
+
+  def edit
+    @license = License.find(params[:id])
+  end
+
+  def update
+    @license = License.find(params[:id])
+    @license.update_attributes(license_params)
+    return redirect_to admin_licenses_path
+  end
+
+  private
+
+  def license_params
+     params.require(:license).permit(:name, :capacity)
+  end
 end
